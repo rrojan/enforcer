8d8f3b2e2f011d1f3adca94f4f6a16dc77f71d9c
Rojan Rajbhandar <rrojan20@gmail.com>
2023-06-02 20:05:59 +0545

    feat: add support for custom error message

--- enforcer/enforcements/match.go
+++ enforcer/enforcements/match.go
@@ -6,27 +6,36 @@ import (
 	"strings"
 )
 
-func matchPattern(pattern, fieldValue, fieldName string) string {
+func matchPattern(pattern, fieldValue, fieldName, customErrorMessage string) string {
 	match, err := regexp.MatchString(pattern, fieldValue)
 	if err != nil {
 		return fmt.Sprintf("Invalid pattern for field '%s'", fieldName)
 	}
 	if !match {
+		if customErrorMessage != "" {
+			return customErrorMessage
+		}
 		return fmt.Sprintf("Field '%s' does not match email pattern", fieldName)
 	}
 	return ""
 }
 
 func HandleMatch(fieldValue, fieldName, opt string) string {
-	var pattern string
+	pattern := ""
+	customErrorMessage := ""
 	switch opt {
 	case "match:email":
 		pattern = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
 	case "match:phone":
 		pattern = `^[0-9\\-]{7,12}$`
+	case "match:password":
+		// At least one uppercase letter, one lowercase letter,
+		// one digit, and one special character
+		pattern = `^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*\p{P})(?!.*\s).*$`
+		customErrorMessage = "Passwords must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
 	default:
 		pattern = strings.TrimPrefix(opt, "match:")
 	}
 
-	return matchPattern(pattern, fieldValue, fieldName)
+	return matchPattern(pattern, fieldValue, fieldName, customErrorMessage)
 }
--- main.go
+++ main.go
@@ -5,30 +5,34 @@ import (
 	"github.com/rrojan/enforcer/enforcer"
 )
 
-type ReqStruct struct {
+type SignupReq struct {
 	Name  string `json:"name" enforce:"required between:2,10"`
 	Email string `json:"email" enforce:"required match:email"`
 	Phone string `json:"phone" enforce:"required match:^[0-9\\-]{7,12}$"`
+	Password string `json:"password" enforce:"min:6 max:64 match:password"`
 }
 
 func main() {
 	router := gin.Default()
 
-	router.POST("/test", func(c *gin.Context) {
-		req := ReqStruct{}
+	// Example usage of enforcer in a generic user signup controller
+	router.POST("/signup", func(c *gin.Context) {
+		req := SignupReq{}
 		if err := c.ShouldBindJSON(&req); err != nil {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
 
+		// enforcer.Validate reads the `enforce:"..."` tags and applies enforcements
 		errors := enforcer.Validate(req)
+
 		if len(errors) > 0 {
 			c.JSON(400, gin.H{"errors": errors})
 			return
 		}
 
 		// Process the valid request here
-		c.JSON(200, gin.H{"message": "Request validates successfully"})
+		c.JSON(200, gin.H{"message": "Signed up user successfully"})
 	})
 
 	router.Run(":6969")
